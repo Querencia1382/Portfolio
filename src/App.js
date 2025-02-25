32953ac94c4b9723e6d671049f3c2206fab4531e
Querencia <shayanmohammadiasl278@gmail.com>
2025-02-25 15:25:14 +0330

    a bit of touch

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>E-commerce</h1>}/>
-        <Route path='/Products' element={<h1>E-commerce</h1>}/>
-        <Route path='/Product/:id' element={<h1>E-commerce</h1>}/>
-        <Route path='/Account' element={<h1>E-commerce</h1>}/>
-        <Route path='/Sign-In' element={<h1>E-commerce</h1>}/>
-        <Route path='/Transactions' element={<h1>E-commerce</h1>}/>
-        <Route path='/Cart' element={<h1>E-commerce</h1>}/>
-        <Route path='/About-us' element={<h1>E-commerce</h1>}/>
-        <Route path='/Contact-us' element={<h1>E-commerce</h1>}/>
+        <Route path='/Products' element={<h1>Products</h1>}/>
+        <Route path='/Product/:id' element={<h1>Product</h1>}/>
+        <Route path='/Account' element={<h1>Account</h1>}/>
+        <Route path='/Sign-In' element={<h1>Sign-In</h1>}/>
+        <Route path='/Transactions' element={<h1>Transactions</h1>}/>
+        <Route path='/Cart' element={<h1>Cart</h1>}/>
+        <Route path='/About-us' element={<h1>About us</h1>}/>
+        <Route path='/Contact-us' element={<h1>Contact us</h1>}/>
       </Routes>
     </BrowserRouter>
   )
