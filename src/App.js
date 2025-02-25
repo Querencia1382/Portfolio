7db621c0c2a85917a9534fb73e5f474b27c14b3c
Querencia <shayanmohammadiasl278@gmail.com>
2025-02-25 16:43:10 +0330

    nav-bar
    set up nav-bar without style

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { BrowserRouter , Routes , Route , Link} from "react-router-dom"
 
 
 export default function App() {
+
+  const [account , setAccount] = React.useState(null)
+
+
   return (
     <BrowserRouter>
+    <nav className='nav-bar'>
+      <Link to="/" className="nav-anchor">Home</Link>
+      <Link to="/Products" className="nav-anchor">Products</Link>
+      <Link to="/Contact-us" className="nav-anchor">Contact-us</Link>
+      <Link to="/About-us" className="nav-anchor">About-us</Link>
+      <Link to="/Cart" className="nav-anchor">Cart</Link>
+      {account != null ? <Link to="/Account" className="nav-anchor">Account</Link> : <Link to="/Sign-in" className="nav-anchor">Log-in</Link>}
+    </nav>
       <Routes>
         <Route path='/' element={<h1>E-commerce</h1>}/>
         <Route path='/Products' element={<h1>Products</h1>}/>
-        <Route path='/Product/:id' element={<h1>Product</h1>}/>
+        <Route path='/Products/:id' element={<h1>Product</h1>}/>
         <Route path='/Account' element={<h1>Account</h1>}/>
-        <Route path='/Sign-In' element={<h1>Sign-In</h1>}/>
+        <Route path='/Sign-in' element={<h1>Sign-in</h1>}/>
         <Route path='/Transactions' element={<h1>Transactions</h1>}/>
         <Route path='/Cart' element={<h1>Cart</h1>}/>
         <Route path='/About-us' element={<h1>About us</h1>}/>
