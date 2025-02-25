47d42b9cb016195ef3bf8ca2c936f4c52360ff43
Querencia <shayanmohammadiasl278@gmail.com>
2025-02-25 15:22:16 +0330

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter , Routes , Route , Link} from "react-router-dom"
 
 
 export default function App() {
   return (
-    <h1>E-commerce</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>E-commerce</h1>}/>
+        <Route path='/Products' element={<h1>E-commerce</h1>}/>
+        <Route path='/Product/:id' element={<h1>E-commerce</h1>}/>
+        <Route path='/Account' element={<h1>E-commerce</h1>}/>
+        <Route path='/Sign-In' element={<h1>E-commerce</h1>}/>
+        <Route path='/Transactions' element={<h1>E-commerce</h1>}/>
+        <Route path='/Cart' element={<h1>E-commerce</h1>}/>
+        <Route path='/About-us' element={<h1>E-commerce</h1>}/>
+        <Route path='/Contact-us' element={<h1>E-commerce</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 };
 
