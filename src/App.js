5db63c228cde0d606e34a32f6fd9cd74eae25d7a
Querencia <shayanmohammadiasl278@gmail.com>
2025-02-25 17:48:28 +0330

    nav structure

--- src/App.js
+++ src/App.js
@@ -14,15 +14,17 @@ export default function App() {
       <Link to="/Products" className="nav-anchor">Products</Link>
       <Link to="/Contact-us" className="nav-anchor">Contact-us</Link>
       <Link to="/About-us" className="nav-anchor">About-us</Link>
-      <Link to="/Cart" className="nav-anchor">Cart</Link>
-      {account != null ? <Link to="/Account" className="nav-anchor">Account</Link> : <Link to="/Sign-in" className="nav-anchor">Log-in</Link>}
+      <div className='nav-container'>
+        <Link to="/Cart" className="nav-anchor">Cart</Link>
+        {account != null ? <Link to="/Account" className="nav-anchor">Account</Link> : <Link to="/Log-in" className="nav-anchor">Log-in</Link>}
+      </div>
     </nav>
       <Routes>
         <Route path='/' element={<h1>E-commerce</h1>}/>
         <Route path='/Products' element={<h1>Products</h1>}/>
         <Route path='/Products/:id' element={<h1>Product</h1>}/>
         <Route path='/Account' element={<h1>Account</h1>}/>
-        <Route path='/Sign-in' element={<h1>Sign-in</h1>}/>
+        <Route path='/Log-in' element={<h1>Log-in</h1>}/>
         <Route path='/Transactions' element={<h1>Transactions</h1>}/>
         <Route path='/Cart' element={<h1>Cart</h1>}/>
         <Route path='/About-us' element={<h1>About us</h1>}/>
