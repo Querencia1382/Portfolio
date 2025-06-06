58c439b28d7e23e77c2cb9c8b234df5cc1117d83
Querencia <shayanmohammadiasl278@gmail.com>
2025-06-07 00:22:53 +0330

    adjusted some style in item component

--- src/Pages/-Components/item.js
+++ src/Pages/-Components/item.js
@@ -8,19 +8,19 @@ export default function Item(props){
 
     return (
         <div className='item'>
-            <img className='item--img' src={props.item.image} alt={props.item.title} width='120px'/>
+            <img className='item--img' src={props.item.image} alt={props.item.title}/>
             <h2 className='item--h2'>{props.item.title}</h2>
             <div className="item--div">
                 {props.discount != false ?
                     <>
                         <h4 className="item--h4 discounted--h4">{props.item.price}$</h4>
-                        <img className="dicsounted--icn" srcSet={rightArrowSvg} width='24px'/>
-                        <Link to={`/Product/${props.item.id}`} className="item--a discounted--a">Only {((100 - props.discount) / 100 * props.item.price).toFixed(2)}$</Link>
+                        <img srcSet={rightArrowSvg} width='24px'/>
+                        <Link to={`/Product/${props.item.id}`} className="item--a">Only {((100 - props.discount) / 100 * props.item.price).toFixed(2)}$</Link>
                     </>
                     :
                     <>
                         <h4 className="item--h4">{props.item.price}$</h4>
-                        <img className="dicsounted--icn" srcSet={rightArrowSvg} width='24px'/>
+                        <img srcSet={rightArrowSvg} width='24px'/>
                         <Link to={`/Product/${props.item.id}`} className="item--a">Order</Link>
                     </>   
             }
