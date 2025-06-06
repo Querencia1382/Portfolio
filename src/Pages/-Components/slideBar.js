594435ded6f219c9da715ef6430595a9dd99641c
Querencia <shayanmohammadiasl278@gmail.com>
2025-06-06 18:12:18 +0330

    finished slideBar

--- src/Pages/-Components/slideBar.js
+++ src/Pages/-Components/slideBar.js
@@ -10,32 +10,7 @@ export default function SlideBar(props){
         queryFn : async () => {
             const request = await fetch(`https://fakestoreapi.com/products`)
             const data = await request.json()
-            if(props.filterBy == true){
-                let filteredList = []
-                for(let i = 0; i < data.length; i++){
-                    if(data[i].category === props.filterBy && data[i].id != props.productID){
-                        filteredList.push(<Item key={data[i].id} item={data[i]} discount={false}/>)
-                    }
-                }
-                return filteredList
-            }
-            else {
-                let counter = 0
-                let displayItems = []
-                let randomValue = 0
-
-                for(let i = 4; i < data.length; i++){
-                    if(counter == 6 || i == data.length - 1){
-                        break
-                    }
-                    randomValue = Math.random() * 10
-                    if(randomValue > 5){
-                        displayItems.push(<Item key={data[i].id} item={data[i]} discount={props.discount == true ?(Math.random() * 40) : false}/>)
-                        counter += 1
-                    }
-                }
-                return displayItems
-            }
+            return data
         }
     })
 
@@ -44,11 +19,38 @@ export default function SlideBar(props){
         return <h2>is Loading...</h2>
     }
     else {
+
+        let displayList = []
+
+
+        if(props.filterBy){
+            for(let i = 0; i < data.length; i++){
+                if(data[i].category === props.filterBy && data[i].id != props.productID){
+                    displayList.push(<Item key={data[i].id} item={data[i]} discount={false}/>)
+                }
+            }
+        }
+        else {
+            let counter = 0
+            let randomValue = 0
+            for(let i = 4; i < data.length; i++){
+                if(counter == 6 || i == data.length - 1){
+                    break
+                }
+                randomValue = Math.random() * 10
+                if(randomValue > 5){
+                    displayList.push(<Item key={data[i].id} item={data[i]} discount={props.discount == true ?(Math.random() * 40) : false}/>)
+                    counter += 1
+                }
+            }
+        }
+
+        
         return (
             <div className="slide--bar">
                 <h2 className="title--h2">{props.title}</h2>
                 <div className="slide--bar--div">
-                    {data}
+                    {displayList}
                 </div>
             </div>
         )
